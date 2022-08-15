16fdb90124f5fbabd44569927e89dc5cad4fc938
mainamirh <mainamirh@Smart-PC>
2022-08-15 11:49:42 +0430

    fully updated dao

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,28 @@
+import sdk from "./1-initialize-sdk.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const token = sdk.getToken(process.env.TOKEN_ADDRESS);
+
+const vote = sdk.getVote(process.env.VOTE_ADDRESS);
+
+(async () => {
+  try {
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [vote.getAddress()] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log(
+      "✅ Successfully revoked our superpowers from the ERC-20 contract"
+    );
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
